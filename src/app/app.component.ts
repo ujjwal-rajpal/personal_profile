73203246efcdfb922f1e178a8ff4f38b754195fe
ujjwal-rajpal <ujjwal.rajpal96@gmail.com>
2019-04-10 13:19:06 +0530

    Implement service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'personal-profile';
+  constructor(public update : SwUpdate){
+
+  }
+  ngOnInit(){
+    this.update.available.subscribe(event=>{
+      this.update.activateUpdate().then(() => document.location.reload());
+    });
+  }
 }
